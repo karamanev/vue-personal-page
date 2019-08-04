8f38e6f4ccc0dc5df6ef64292b02a84563de15a1
karamanev <karamanev@gmail.com>
2019-08-04 15:46:25 +0300

    feat: Routing initialization

---
+++ src/admin/authentication/Login.vue
@@ -0,0 +1,23 @@
+<template>
+      <div class="login">
+      <h1>Login</h1>
+      <form>
+        <label for="login">Username</label>
+        <input type="text" id="login" placeholder="Ivan Ivanov" />
+        <label for="password">Password</label>
+        <input type="password" id="password" placeholder="******" />
+
+        <input type="submit" value="Login" />
+      </form>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/admin/authentication/Register.vue
@@ -0,0 +1,25 @@
+<template>
+        <div class="register">
+      <h1>Register</h1>
+      <form>
+        <label for="username">Username</label>
+        <input type="text" id="username" placeholder="Ivan Ivanov" />
+        <label for="email">Email</label>
+        <input type="text" id="email" placeholder="ivan@gmail.com" />
+        <label for="password">Password</label>
+        <input type="password" id="password" placeholder="******" />
+
+        <input type="submit" value="Register" />
+      </form>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
