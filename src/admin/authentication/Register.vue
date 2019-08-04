68073e89a5bcc3641833a38c5d99a55d65129cd6
karamanev <karamanev@gmail.com>
2019-08-04 22:54:32 +0300

    Base pages

--- src/admin/authentication/Register.vue
+++ src/admin/authentication/Register.vue
@@ -1,21 +1,40 @@
 <template>
         <div class="register">
       <h1>Register</h1>
-      <form>
+      <form @submit.prevent="OnRegisterClick">
         <label for="username">Username</label>
-        <input type="text" id="username" placeholder="Ivan Ivanov" />
+        <input type="text" v-model="$v.username.$model" id="username" placeholder="Ivan Ivanov" />
         <label for="email">Email</label>
-        <input type="text" id="email" placeholder="ivan@gmail.com" />
+        <input type="text" v-model="$v.email.$model" id="email" placeholder="ivan@gmail.com" />
         <label for="password">Password</label>
-        <input type="password" id="password" placeholder="******" />
-
-        <input type="submit" value="Register" />
+        <input type="password" v-model="$v.password.$model" id="password" placeholder="******" />
+        <input type="submit" value="Register" :disabled="$v.$error" />
       </form>
     </div>
 </template>
 
 <script>
+import {required, minLength, maxLength, email} from 'vuelidate/lib/validators';
+
 export default {
+  data() {
+      return {
+        username: '',
+        password: '',
+        email: '',
+      }
+  },
+  validations: {
+      username: {required, minLength: minLength(3), maxLength: maxLength(20)},
+      email: {required, minLength: minLength(3), maxLength: maxLength(20), email},
+      password: {required}
+  },
+    methods: {
+    OnRegisterClick () {
+       console.log('a');
+       
+    }
+  },
 
 }
 </script>
