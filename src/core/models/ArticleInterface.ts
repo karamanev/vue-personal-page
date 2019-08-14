ad2ba8f2c1dc12e406ba9a2cbffaa3f8a54861da
Georgi Karamanev <karamanev@gmail.com>
2019-08-14 09:22:56 +0300

    Added article interface

---
+++ src/core/models/ArticleInterface.ts
@@ -0,0 +1,8 @@
+export default interface Article {
+    title: string;
+    subtitle: string;
+    images: [string];
+    innerTitles: [string];
+    imageTexts?: [string];
+    quotes: [string];
+}
