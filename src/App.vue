4ba8e00c1bc6799198df270b21aa6a917f7bd89f
karamanev <karamanev@gmail.com>
2019-09-07 15:37:58 +0300

    Initial Webpack organization

--- src/App.vue
+++ src/App.vue
@@ -1,31 +1,10 @@
 <template>
-  <v-app id="app">
-    <page-header app/>
-    <v-content class="mt-12">
-      <v-container fluid>
-        <router-view/>
-      </v-container>
-    </v-content>
-    <page-footer app/>
-  </v-app>
+  <div>
+    <router-link to="/">
+      Home
+    </router-link> | <router-link to="/medium">
+      Medium
+    </router-link>
+    <router-view />
+  </div>
 </template>
-
-<script lang="ts">
-import Vue from 'vue'
-import firebase from 'firebase'
-import PageHeader from './components/common/header/PageHeader.vue'
-import PageFooter from './components/common/footer/PageFooter.vue'
-
-export default Vue.extend({
-  components: {
-    PageHeader,
-    PageFooter
-  }
-
-})
-</script>
-
-<style lang="scss">
-  @import './scss/fonts.scss';
-  @import './scss/variables.scss';
-</style>
--- src/core/plugins/theme.ts
+++
@@ -1,8 +0,0 @@
-export default {
-  first: '#003B6D',
-  second: '#6699CC',
-  third: '#EBEDF3',
-  forth: '#BDBDBD',
-  fifth: '#676767',
-  primary: '#BDBDBD'
-}
